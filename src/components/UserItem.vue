22033911207740480ad660aad88a7627eb25e4c2
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-12 11:56:27 +0330

    create USeritem compo

---
+++ src/components/UserItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="user-box">
+    <h3>
+      <span class="font-bold">userName : </span>user 1
+    </h3>
+    <h3>
+      <span class="font-bold">job : </span>frontend developer
+    </h3>
+    <h3>
+      <span class="font-bold">role : </span>admin
+    </h3>
+    <h3>
+      <span class="font-bold">city : </span>gonbad kavus
+    </h3>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  item: Object
+})
+
+console.log('props :', props)
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
