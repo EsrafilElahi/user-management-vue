d070e141d9a7a0f4fb7eacff09b6d2f9348a9546
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-12 10:29:51 +0330

    fix active style filter navbar

--- src/components/FilterNav.vue
+++ src/components/FilterNav.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="flex justify-center gap-3 mb-10">
-    <RouterLink to="/">USER</RouterLink> |
-    <RouterLink to="/users/add">ADD USER</RouterLink>
+    <RouterLink active-class="text-red-500" class="font-bold" to="/">USER</RouterLink> |
+    <RouterLink active-class="text-red-500" class="font-bold" to="/users/add">ADD USER</RouterLink>
   </div>
 </template>
 
 <script setup>
 import { watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 
+const router = useRouter()
 const route = useRoute()
+
+console.log(router)
+console.log(route.name)
 
 watch(route, (to, from) => {
   console.log('Route changed:', to.path, '--->', from.path);
